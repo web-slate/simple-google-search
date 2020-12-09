ae1d998e2a1fc2d94801c2886de626c4b9f41895
Venkat.R <ramsunvtech@gmail.com>
2020-12-10 01:34:29 +0800

    tech(base): Add Spring Boot Base Setup

---
+++ api/google-search/src/main/java/com/google/search/googlesearch/GoogleSearchApplication.java
@@ -0,0 +1,13 @@
+package com.google.search.googlesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleSearchApplication.class, args);
+	}
+
+}
