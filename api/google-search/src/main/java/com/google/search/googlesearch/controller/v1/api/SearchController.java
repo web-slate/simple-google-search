6c9bfe3893bc359a3bd582280c8b87756e199fe9
Naveen Vignesh <naveenvignesh5.nv@gmail.com>
2020-12-11 18:55:36 +0530

    feat(api/swagger): #11 - Adds basic swagger annotation with a sample rest api

---
+++ api/google-search/src/main/java/com/google/search/googlesearch/controller/v1/api/SearchController.java
@@ -0,0 +1,21 @@
+package com.google.search.googlesearch.controller.v1.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+public class SearchController {
+    @GetMapping(value = "/api/v1/search")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Get search results", content = @Content(schema = @Schema(nullable = true)))
+    })
+    public String getSearch(@RequestParam(defaultValue = "param") String query) {
+        return "Query: " + query;
+    }
+}
