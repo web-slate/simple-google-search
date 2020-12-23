c760129e1f238636b66ec4bc8f23ead301ecab10
Jayaprakash Kulandaivel <j.prakashuk@gmail.com>
2020-12-23 15:11:21 +0100

    add search

---
+++ client/google-search-app/src/app/app-body/app-body.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AppBodyComponent } from './app-body.component';
+
+describe('AppBodyComponent', () => {
+  let component: AppBodyComponent;
+  let fixture: ComponentFixture<AppBodyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AppBodyComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppBodyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
