66d9b30af285442c4f78d674220bb53891c6cada
Jayaprakash Kulandaivel <j.prakashuk@gmail.com>
2020-12-23 15:11:21 +0100

    add search

--- client/google-search-app/src/app/app.module.ts
+++ client/google-search-app/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchResultComponent } from './search-result/search-result.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
+import { AppBodyComponent } from './app-body/app-body.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    SearchResultComponent,
+    SearchResultsComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppBodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
